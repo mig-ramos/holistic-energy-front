4e33d90eb62612b7caea11c17245281966bc14f7
Miguel Arcanjo <mig.ramos10@gmail.com>
2023-12-29 20:56:31 -0300

    feat: Cria paginas de Login e Register

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,71 @@
+import { Link } from "react-router-dom";
+import logoImg from "../../assets/Logo-thGreen.svg";
+import { Container } from "../../components/container";
+import { Input } from "../../components/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 export function Login() {
-    return (
-      <div>
-        <h1>Login</h1>
-      </div>
-    );
+  const schema = z.object({
+    email: z
+      .string().min(1, "O campo email é obrigatório")
+      .email("Insira um email válido"),
+    password: z
+    .string().min(6, "O campo senha é obrigatório | mín. 6 caracteres"),
+  });
+
+  type FormData = z.infer<typeof schema>;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    mode: "onChange",
+  });
+
+  function onSubmit(data: FormData) {
+    console.log(data);
   }
+  return (
+    <Container>
+      <div className="w-full min-h-screen flex justify-center items-center flex-col gap-4">
+        <Link to={"/"} className="mb-6 max-w-sm w-full">
+          <img src={logoImg} alt="Logo do site" className="w-full" />
+        </Link>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="bg-white max-w-xl w-full rounded-lg p-4"
+        >
+          <div className="mb-3">
+            <Input
+              type="email"
+              placeholder="Digite seu email..."
+              name="email"
+              error={errors.email?.message}
+              register={register}
+            />
+          </div>
+          <div className="mb-3">
+            <Input
+              type="password"
+              placeholder="Digite sua senha..."
+              name="password"
+              error={errors.password?.message}
+              register={register}
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-zinc-900 w-full rounded-md text-white h-10 font-medium"
+          >
+            Acessar
+          </button>
+        </form>
+        <Link to={"/register"}>Ainda não possui uma conta? Cadastre-se!</Link>
+      </div>
+    </Container>
+  );
+}
