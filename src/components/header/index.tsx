1ee1123cee9d9cae118983df1de7aaab1f9f5637
Miguel Arcanjo <mig.ramos10@gmail.com>
2023-12-29 16:15:07 -0300

    feat: Estiliza Header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,32 @@
+import { Link } from "react-router-dom";
+import logoImg from "../../assets/Logo-thGreen.svg";
+import { FiUser, FiLogIn } from "react-icons/fi";
+
 export function Header() {
-    return (
-      <div>
-        <h1>Header</h1>
-      </div>
-    );
-  }
+  const signed = false;
+  const loadingAuth = false;
+
+  return (
+    <div className="w-full flex items-center h-16 bg-white drop-shadow mb-4">
+      <header className="flex w-full max-w-7xl items-center justify-between px-4 mx-auto">
+        <Link to={"/"}>
+          <img src={logoImg} alt="Logo do site" className="h-14" />
+        </Link>
+        {!loadingAuth && signed && (
+          <Link to={"/dashboard"}>
+            <div className="border-2 rounded-full p-1 border-gray-900">
+              <FiUser size={24} color="#000" />
+            </div>
+          </Link>
+        )}
+        {!loadingAuth && !signed && (
+          <Link to={"/login"}>
+            <div className="border-2 rounded-full p-1 border-gray-900">
+              <FiLogIn size={24} color="#000" />
+            </div>
+          </Link>
+        )}
+      </header>
+    </div>
+  );
+}
